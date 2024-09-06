c6e4258353efb9b8137d9793debfc5189ff6c5c3
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-09-05 21:14:43 -0500

    feat: :sparkles: Add index.html for maps

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-from routers import department, city, location
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-app.include_router(department.router)
-app.include_router(city.router)
-app.include_router(location.router)
+# Montar la carpeta "static" para servir el index.html
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Servicio que devuelve los datos de ubicación (Ejemplo para Bogotá)
+@app.get("/api/location")
+async def get_location(place: str):
+    if place == "Bogota Cundinamarca":
+        return JSONResponse(content={
+            "success": True,
+            "data": {
+                "latitude": 4.91857,
+                "longitude": -74.02799,
+                "altitude": 2559.0
+            },
+            "count": 1
+        })
+    else:
+        return JSONResponse(content={"success": False, "message": "Location not found"})
